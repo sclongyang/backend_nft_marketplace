98a9b7d8a0174362a9c856df01806355fdc2cb02
sclongyang@126.com <sclongyang@126.com>
2022-10-02 12:37:45 +0800

    update frontend file

---
+++ deploy/03_update_front_end.js
@@ -0,0 +1,44 @@
+const fs = require("fs")
+const { ethers, network } = require("hardhat")
+const { frontEndABIDir, frontEndContractsAddressFile,frontEndContractsAddressFile2, frontEndABIDir2 } = require("../helper-hardhat-config")
+
+require("dotenv").config()
+
+
+
+module.exports = async () => {
+    if (process.env.UPDATE_FRONTEND) {
+        console.log("start update frontend file")
+        await UpdateABIs()
+        await UpdateContractsAddress()
+        console.log("succ update frontend file")
+    }
+}
+
+async function UpdateABIs() {
+    const baseNFT = await ethers.getContract("BaseNFT")
+    fs.writeFileSync(`${frontEndABIDir}BaseNFT.json`, baseNFT.interface.format(ethers.utils.FormatTypes.json))
+    fs.writeFileSync(`${frontEndABIDir2}BaseNFT.json`, baseNFT.interface.format(ethers.utils.FormatTypes.json))
+
+    const nftMarketplace = await ethers.getContract("NFTMarketplace")
+    fs.writeFileSync(`${frontEndABIDir}NFTMarketplace.json`, nftMarketplace.interface.format(ethers.utils.FormatTypes.json))
+    fs.writeFileSync(`${frontEndABIDir2}NFTMarketplace.json`, nftMarketplace.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function UpdateContractsAddress() {
+    const chainId = network.config.chainId.toString()
+    const contractName = "NFTMarketplace"
+    const nftMarketplace = await ethers.getContract(contractName)
+    const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsAddressFile, "utf8"))
+    if(chainId in contractAddresses){
+        if( !contractAddresses[chainId][`${contractName}`].includes(nftMarketplace.address)){
+            contractAddresses[chainId][`${contractName}`].push(nftMarketplace.address)
+        }
+    }else{
+        contractAddresses[chainId] = {"NFTMarketplace":[nftMarketplace.address]}
+    }    
+    fs.writeFileSync(frontEndContractsAddressFile, JSON.stringify(contractAddresses))
+    fs.writeFileSync(frontEndContractsAddressFile2, JSON.stringify(contractAddresses))
+}
+
+module.exports.tags = ["all", "frontend"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,9 +15,17 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsAddressFile = "../frontend_nft_marketplace_moralis/constants/networkMapping.json"
+const frontEndContractsAddressFile2 = "../frontend_nft_marketplace_subgraph/constants/networkMapping.json"
+const frontEndABIDir = "../frontend_nft_marketplace_moralis/constants/"
+const frontEndABIDir2 = "../frontend_nft_marketplace_subgraph/constants/"
 
 module.exports = {
     networkConfig,
     developmentChains,
     VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsAddressFile,
+    frontEndContractsAddressFile2,
+    frontEndABIDir,
+    frontEndABIDir2,
 }
